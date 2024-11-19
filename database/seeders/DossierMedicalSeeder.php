638c13bf7e30a27c4a3d8028b2dbc1c7c7c8b252
loctetgenius <sagnamoussa546@gmail.com>
2024-11-19 13:54:40 +0000

    Rest Of Fonctionnality

--- database/seeders/DossierMedicalSeeder.php
+++ database/seeders/DossierMedicalSeeder.php
@@ -14,32 +14,6 @@ class DossierMedicalSeeder extends Seeder
      */
     public function run(): void
     {
-         // Dossiers médicaux pour le patient 1
-         for ($i = 1; $i <= 5; $i++) {
-            DossierMedicaux::create([
-                'patient_id' => 1, // ID du premier patient
-                'numero_dme' => Str::random(10), // Génère un numéro DME aléatoire
-                'date_creation' => now()->subDays(rand(1, 30)), // Date de création dans le passé
-                'antecedents_medicaux' => "Antécédents médicaux pour le patient 1, dossier $i",
-                'traitements' => "Traitements pour le patient 1, dossier $i",
-                'notes_observations' => "Notes et observations pour le patient 1, dossier $i",
-                'intervention_chirurgicale' => "Intervention chirurgicale pour le patient 1, dossier $i",
-                'info_sup' => "Informations supplémentaires pour le patient 1, dossier $i",
-            ]);
-        }
-
-        // Dossiers médicaux pour le patient 2
-        for ($i = 1; $i <= 5; $i++) {
-            DossierMedicaux::create([
-                'patient_id' => 2, // ID du deuxième patient
-                'numero_dme' => Str::random(10), // Génère un numéro DME aléatoire
-                'date_creation' => now()->subDays(rand(1, 30)), // Date de création dans le passé
-                'antecedents_medicaux' => "Antécédents médicaux pour le patient 2, dossier $i",
-                'traitements' => "Traitements pour le patient 2, dossier $i",
-                'notes_observations' => "Notes et observations pour le patient 2, dossier $i",
-                'intervention_chirurgicale' => "Intervention chirurgicale pour le patient 2, dossier $i",
-                'info_sup' => "Informations supplémentaires pour le patient 2, dossier $i",
-            ]);
-        }
+        //
     }
 }
